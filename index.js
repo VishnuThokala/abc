8e04d71ac9d047fd6ad6c66b7259d64947349957
Thokala Vishnu Vardhan Reddy <46997376+VishnuThokala@users.noreply.github.com>
2021-05-31 14:18:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const adminRoute = require('./routes/admin');
 var sendVerificationEmail = require('../functions/utils/emailUtil')
 
 firebase.initializeApp(config);
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use('/', userAuthRoute);
 app.use('/', adminAuthRoute);
